3235300562106302245e4729ccedd47ef90c1fbd
Naitik Rathod <naitikrathod@Naitiks-MacBook-Air.local>
2024-05-09 21:30:49 -0400

    model added to backend and connected to frontend

--- backend/base.py
+++ backend/base.py
@@ -4,21 +4,20 @@ from flask_cors import CORS
 api = Flask(__name__)
 CORS(api)
 
+import google.generativeai as genai
+
+for i, m in zip(range(5), genai.list_tuned_models()):
+  print(m.name)
+
+model = genai.GenerativeModel(model_name=f'tunedModels/gemini1tuned2-xxr5kvkgzqhb')
+
+
 @api.route('/search', methods=['POST'])
 def search():
     data = request.get_json()  # Get data from POST request
     query = data.get('query')  # Access the search query from the data
     # Perform search or handle the query here
-    print("data : ", data)
-    print("query ::: ", query)
-    return jsonify({'status': 'success', 'response': query})
+    result = model.generate_content(query)
 
-@api.route('/profile')
-def my_profile():
-    response_body = {
-        "name": "Nagato",
-        "about" :"Hello I'm a full stack developer that loves python and javascript"
-    }
-
-    return response_body
+    return jsonify({'status': 'success', 'response': result.text})
 
